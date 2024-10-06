2aa98029cbdcb601db700d8766ab7cfc23c5bae7
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-10-05 22:05:15 -0500

    Add Chapter: Images, Input and Collisions

--- game.py
+++ game.py
@@ -9,12 +9,42 @@ class Game:
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
 
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
+
+        self.img_pos = [160, 260]
+        self.movements = [False, False]
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+
+            img_r = pygame.Rect(
+                self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255),
+                                 self.collision_area)
+
+            self.img_pos[1] += (self.movements[1] - self.movements[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movements[0] = True
+                    elif event.key == pygame.K_DOWN:
+                        self.movements[1] = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movements[0] = False
+                    elif event.key == pygame.K_DOWN:
+                        self.movements[1] = False
 
             pygame.display.update()
             self.clock.tick(60)
